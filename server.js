502a720f2263aa592a09315a48e5373e1e6b1f04
Brian Piltin <blpiltin@gmail.com>
2021-11-13 15:58:08 -0600

    Add urls for reading and writing high scores

--- server.js
+++ server.js
@@ -1,17 +1,64 @@
-const express = require('express');
+/*
+* server.js
+* 
+* The high score server REST interface. 
+* There are 2 URLS that both use get, one to query a high score and the other to post it.
+*
+* Author: Brian Piltin
+* Copyright: Copyright (C) 2021 Brian Piltin. All Rights Reserved.
+*/
+
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
-const fs = require('fs');
+const fs = require("fs");
 
-app.get('/', (req, res) => {
-    res.end('Welcome to server02x.');
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.end("Welcome to server02x.");
 });
 
-app.get('/list_movies', (req, res) => {
-    fs.readFile(__dirname + '/movies.json', 'utf8', (err, data) => {
-        res.end(data);
+app.get("/high_scores/:game", (req, res) => {
+    fs.readFile(__dirname + "/scores.json", "utf8", (err, data) => {
+        data = JSON.parse(data);
+        let obj = data.find(obj => obj.game === req.params.game);
+        if (obj) {
+            res.send({ name: obj.name, score: obj.score });
+        } else {
+            res.send({ error: "Game \"" + req.params.game + "\" not found." } );
+        }
     });
 })
+
+app.get("/high_scores/:game/update/:name/score/:score", (req, res) => {
+    if (isNaN(req.params.score)) {
+        res.send({ error: "Invalid request." });
+        return;
+    }
+    fs.readFile(__dirname + "/scores.json", "utf8", (err, data) => {
+
+        data = JSON.parse(data);
+        let obj = data.find(obj => obj.game === req.params.game);
+
+        if (!obj) {
+            res.send({ error: "Game \"" + req.params.game + "\" not found." } );
+        }
+
+        let oldScore = parseInt(obj.score);
+        let newScore = parseInt(req.params.score);
+
+        if (newScore > oldScore) {
+            obj.name = req.params.name;
+            obj.score = newScore;
+            fs.writeFile(__dirname + "/scores.json",
+                JSON.stringify(data), () => res.send({ success: "Score updated." }));
+        } else {
+            res.send({ message: "Score not updated." });
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
